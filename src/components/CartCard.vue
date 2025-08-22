afdcac4670a10f155d3e232361c9664708e05d7f
DaRon-HomePC <max99124@gmail.com>
2025-08-23 02:18:23 +0800

    1.新增專案 2.新增main.vue為原始範例專案 3.新增ProductCard，可在APP中做前後傳輸，把drink加入cart

---
+++ src/components/CartCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+
+const emit = defineEmits(['emit-text'])
+const text = ref('')
+
+const send = () => {
+  emit('emit-text', text.value) // 傳出字串參數
+}
+
+</script>
+
+<template>
+  <div style="border: 1px solid #ddd; padding: 8px; border-radius: 6px">
+    <input v-model="text" placeholder="輸入新標題..." />
+    <button @click="send">套用到父層</button>
+  </div>
+</template>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <a v-for="drink in drinks" :key="drink.id" href="#" class="list-group-item list-group-item-action"
+        @click.prevent="addToCart(drink)">
+        <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">{{ drink.name }}</h5>
+            <small>${{ drink.price }}</small>
+        </div>
+        <p class="mb-1">{{ drink.description }}</p>
+    </a>
+    <!-- <h3>{{props.title}}</h3> -->
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+//傳入的商品
+const props = defineProps({
+    drinks: { type: Object, require: true } //原本變數就是drinks，偷懶就直接傳入Object不改了XD
+})
+
+const emit = defineEmits(['cart-item'])
+const text = ref('');
+
+const addToCart = (drink) => {
+    emit('cart-item', drink) // 傳出飲料物件給App
+    console.log(`準備從子物件傳出的 ID : ${x.id}`);
+}
+
+</script>
